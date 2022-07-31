ae4d00b3b0597f70826b8c5aacc80e54cd8d54db
Your Name <you@example.com>
2022-07-31 10:22:30 -0400

    copied objec1 into obj2

--- ObjCopy.js
+++ ObjCopy.js
@@ -18,3 +18,13 @@ console.log(object1.hobby);
 
 //accessing element (cinema) in propety hobby
 console.log(object1.hobby[3]);
+
+//copy one object to another using Object.assign methd
+
+object2 = {
+  "man of the day": "not a legal naming",
+  journal: ["eating", "reding", "working"],
+  event: false,
+};
+let objectCopied = Object.assign(object1, object2);
+console.log(objectCopied);
